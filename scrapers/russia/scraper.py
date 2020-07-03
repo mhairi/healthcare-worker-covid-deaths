fbd13969a3fdfe11097a256721de48992a36fb5e
Mhairi McNeill <mhairihmcneill@gmail.com>
2020-07-03 15:02:30 +0100

    Another restructure, re-running scrape, adding more details to scrape, some data cleaning

--- scrapers/russia/scraper.py
+++ scrapers/russia/scraper.py
@@ -1,21 +1,16 @@
-#!/usr/bin/env python
-# coding: utf-8
-# %%
 import requests
 from csv import DictWriter
 from bs4 import BeautifulSoup
 from datetime import date
 
 
-# %%
 # Get data from website
 url = "https://sites.google.com/view/covid-memory/home"
 request = requests.get(url)
 html = request.content
-soup = BeautifulSoup(html)
+soup = BeautifulSoup(html, features="lxml")
 
 
-# %%
 # Two lists on the page: get al elements from both
 lists = soup.find_all("ol", class_ = "n8H08c BKnRcf")
 
@@ -26,25 +21,14 @@ output_1 = [{"data":element.text, "country":"Russia"} for element in russian_lis
 output_2 = [{"data":element.text, "country":"Other"} for element in other_list]
 outputs = output_1 + output_2
 
-
-# %%
 # File will be named after the date of the scrape
 today = date.today()
 today = today.strftime("%Y-%m-%d")
 file_name = "data/" + today + ".csv"
 
-
-# %%
 # Write to CSV
 with open(file_name, "w") as f:
     field_names = ["data", "country"]
     writer = DictWriter(f, fieldnames = field_names)
     writer.writeheader()
     writer.writerows(outputs)
-
-
-# %%
-
-
-
-
